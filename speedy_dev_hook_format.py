b1bd5be169f2261897fc1e55acad3e04676ed996
Mebrahtom Kidane <mebrahtomkg1@gmail.com>
2025-09-04 15:19:57 +0300

    feat: implement format command fully

--- speedy_dev_hook_format.py
+++ speedy_dev_hook_format.py
@@ -1,9 +1,27 @@
 import sublime
 import sublime_plugin
-import urllib.request
-import urllib.error
+from .api_client import call_api
 
 
 class SpeedyDevHookFormatCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        url = f"http://localhost:{3000}/format"
+        region = sublime.Region(0, self.view.size())
+        code = self.view.substr(region)
+
+        file_path = self.view.file_name()
+
+        data = {
+            "filepath": file_path,
+            "content": code,
+        }
+
+        try:
+            formatted = call_api("/format", method="POST", data=data)
+            if isinstance(formatted, str):
+                self.view.replace(edit, region, formatted)
+                print("[speedy-dev-hook] File formatted successfully.")
+            else:
+                raise Exception("[speedy-dev-hook] Invalid format response!")
+
+        except Exception as e:
+            sublime.error_message(str(e))
