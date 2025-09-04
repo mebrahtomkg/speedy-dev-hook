2b175dd9c3f971ebcf823b55424fc34fbf82fb69
Mebrahtom Kidane <mebrahtomkg1@gmail.com>
2025-09-04 12:20:10 +0300

    feat: use config.json file to configure supported extensions for formatting

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,1 +1,28 @@
+import { readFile } from 'node:fs/promises';
+import path from 'node:path';
+
 export const PORT = 3000;
+
+export const MAX_TXT_SIZE = 1 * 1024 * 1024; // 1MB
+
+const CONFIG_FILE_PATH = path.join(
+  path.dirname(new URL(import.meta.url).pathname),
+  '../../config.json',
+);
+
+interface AppConfig {
+  supportedExtensions: string[];
+}
+
+let config: AppConfig;
+
+try {
+  const data = await readFile(CONFIG_FILE_PATH, 'utf8');
+
+  config = JSON.parse(data);
+} catch (error) {
+  console.error('Failed to load config file: ', error);
+  process.exit(0);
+}
+
+export const PRETTIER_SUPPORTED_EXTS = config.supportedExtensions;
