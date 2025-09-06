34b2136647b06bfa97282ace53ed43cae9ec8e05
Mebrahtom Kidane <mebrahtomkg1@gmail.com>
2025-09-06 22:56:44 +0300

    feat: allow user to run a script from the listed scripts

--- speedy_dev_hook_list_scripts.py
+++ speedy_dev_hook_list_scripts.py
@@ -7,17 +7,18 @@ from .api_client import call_api
 class SpeedyDevHookListScriptsCommand(sublime_plugin.WindowCommand):
     def run(self, paths=[]):
         self.scripts = []
-        # Get all folders in the current Sublime Text project
-        folders = self.window.folders()
 
-        if not folders:
+        # Get all folders in the current Sublime Text project
+        self.folders = self.window.folders()
+
+        if not self.folders:
             # If no project folders are open, inform the user and exit.
             sublime.error_message("No project folders open!")
             return
 
         try:
             start_time = int(time.time() * 1000)
-            self.scripts = call_api(f"/scripts?folderpath={folders[0]}")
+            self.scripts = call_api(f"/scripts?folderpath={self.folders[0]}")
             end_time = int(time.time() * 1000)
             print(
                 f"[speedy-dev-hook] Scripts list fetched successfully.({end_time-start_time}ms)"
@@ -28,4 +29,6 @@ class SpeedyDevHookListScriptsCommand(sublime_plugin.WindowCommand):
             sublime.error_message(str(e))
 
     def on_script_selected(self, index):
-        print(f"[speedy-dev-hook] Selected script: {self.scripts[index]}")
+        call_api(
+            f"/run-script?folderpath={self.folders[0]}&script={self.scripts[index]}"
+        )
